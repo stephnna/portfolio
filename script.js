222da669ca8b8b73d3abc0a72300af8a46dc518c
stephnna <stephennnabuike@gmail.com>
2022-05-14 11:50:03 +0100

    Update script file

--- script.js
+++ script.js
@@ -9,115 +9,119 @@ hamBurger.addEventListener('click', () => {
 // creating first card item i.e grid project
 
 
-
-
-// const projectsContainer = document.createElement("div");
-// const projectName = document.createElement("h3");
-
-// sectionElement.appendChild(projectsContainer);
-// sectionElement.className = "container-recent";
-// projectsContainer.id = "multi-post-id";
-// projectsContainer.className = "grid-project";
-
-// const multiPostElement = projectsContainer.cloneNode(true);
-// const multiPostElementChild = document.getElementById("multi-post-id");
-// multiPostElementChild.appendChild(multiPostElement);
-// multiPostElement.id =  "multiPostChildId";
-// multiPostElement.className = "multi-post";
-// projectName.innerHTML =   "Multi-Post Stories" + "<br />" + "Gain+Glory";
-// projectName.id =  "multi-text-id";
-// projectName.className = "multi-text-color";
-// document.getElementById("multiPostChildId").appendChild(projectName );
-
-// ulLanguage = document.createElement("ul");
-// document.getElementById("multiPostChildId").appendChild(ulLanguage);
-// ulLanguage.className = ("language display-flex");
-// ulLanguage.id = "ul-tech-id"
-// listItem = document.createElement("li");
-// document.getElementById("ul-tech-id").appendChild(listItem);
-// listItem.className = "language-background";
-// listItem.innerHTML = "Ruby on rails";
-// listItemSecond = listItem.cloneNode(true);
-// document.getElementById("ul-tech-id").appendChild(listItemSecond);
-// listItemSecond.innerHTML = "css";
-// listItemthird = listItem.cloneNode(true);
-// document.getElementById("ul-tech-id").appendChild(listItemthird);
-// listItemthird.innerHTML = "JavaScript";
-// listItemfourth = listItem.cloneNode(true);
-// document.getElementById("ul-tech-id").appendChild(listItemfourth);
-// listItemfourth.innerHTML = "html";
-
-// seeProjectButton = document.createElement("button");
-// seeProjectButton.innerHTML = "See Project";
-// seeProjectButton.className = "see-project";
-// seeProjectButton.id = "button-id";
-// document.getElementById("multiPostChildId").appendChild(seeProjectButton);
-// document.getElementById("button-id").setAttribute("type", "button"); 
-
-// // end creating first card item i.e grid project
-
-// // creating objets that holds the card information
-
 const project1 = {
-  name: 'Project 1',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',
 };
 
 const project2 = {
-  name: 'Project 2',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',
 };
 
 const project3 = {
-  name: 'Project 3',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',
 };
 
 const project4 = {
-  name: 'Project 4',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',
   
 };
 
-
 const project5 = {
-  name: 'Project 5',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',
 };
 
 const project6 = {
-  name: 'Project 6',
-  description: 'Multi-Post Stories<br>Gain+Glory',
-  image: '',
-  technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',  
+    name: 'Keeping track of hundreds of components',
+    description: 'Multi-Post Stories<br>Gain+Glory',
+    image: '',
+    // technologies: ['Ruby on rails', 'css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap', 'Terminal', 'Codepen'],
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'css',
+    jsSkill: 'JavaScript',
+    htmlSkill: 'html',
+    CodeKit: 'Codekit',
+    gitHub: 'GitHub',
+    boostrap: 'Boostrap',
+    terminal: 'Terminal',
+    codePen: 'Codepen',
+    seeProject: 'See Project',
+    live: 'https://stephnna.github.io/portfolio/',
+    source: 'https://github.com/stephnna/portfolio',  
 };
 
 // creating objets that holds the card information
@@ -125,46 +129,62 @@ const project6 = {
 const projects = [project1, project2, project3, project4, project5, project6];
 
 const sectionElement = document.getElementById("projects-container");
-// ${worksInfo[index].cardDisplay}
+sectionElement.className = "container-recent";
+
+for(i = 0; i < projects.length; i++){
+const gridProject = document.createElement('div');
+gridProject.className = "grid-project";
+sectionElement.appendChild(gridProject);
+}
+
 
 Array.from(sectionElement.children).forEach((child, index) => {
-  sectionElement.children.classList = "grid-project";
   child.innerHTML = `
     <div class="multi-post">
-      <h3 class="multi-text-color">${projects[index].description}</h3>
+    <h3 class="multi-text-color">${projects[index].description}</h3>
      <ul class="language display-flex">
-     <li class="language-background">Ruby on rails</li>
-     <li class="language-background">css</li>
-     <li class="language-background" >JavaScript</li>
-     <li class="language-background">html</li>    
-   </ul>  
-   <button class="see-project" type="button">See Project</button>
+     <li class="language-background">${projects[index].rubySkill}</li>
+     <li class="language-background">${projects[index].cssSkill}</li>
+     <li class="language-background">${projects[index].jsSkill}</li>
+     <li class="language-background">${projects[index].htmlSkill}</li> 
+     </ul>   
+    <button class="see-project" type="button">${projects[index].seeProject}</button>
  </div>
- `;
+`;
 });
 
+// modal design begins
 
 
-
-
-// // clone the grid projects into 5 times, the first one was created at card item 
-
-// for(i = 0; i < projects.length; i++){
-//   const gridProject = document.getElementById("multi-post-id").cloneNode(true);
-//   document.getElementById("projects-container").appendChild(gridProject);
-//   projectName.innerHTML = projects[i].description;
-//   seeProjectButton.innerHTML = projects[i].seeProject;
-//   seeProjectButton.addEventListener("click", displayDate); 
-//   // document.getElementById("button-id").addEventListener("click", displayDate);
-// // gridProject.
-//   }
-
-// //  document.getElementById("button-id").addEventListener("click", displayDate);
-// // gridProject.
-
-// function displayDate() {
-//   console.log("cliked");
-// }
-
-
-
+//  Display modal on button click
+const seeProjectVar = document.querySelectorAll('.see-project');;
+seeProjectVar.forEach((item) => {  
+  item.addEventListener('click', () => {
+    const modalContainer = document.createElement('section');
+    modalContainer.className = 'modal-container';
+    modalContainer.id = "modal-container-id";
+    modalContainer.innerHTML = `
+    <img src="" alt="">
+ <h2>hhhhhhhhhhhh</h2>
+ <ul class="modal-skill display-flex">
+  <li class="language-background">Ruby on rails</li>
+  <li class="language-background">css</li>
+  <li class="language-background" >JavaScript</li>   
+</ul>
+<p class="mobile-modal-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+  Quae modi inventore ex aut voluptatum eaque a repellendus animi dolore sed 
+  temporibus autem amet quod, tempore architecto vitae aspernatur minima aliquid 
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt amet eligendi 
+  voluptatum mollitia autem, veritatis dicta doloremque nulla quibusdam,iusto 
+  consequatur eum ad temporibus autem amet quod, tempore architecto vitae aspernatur 
+  minima aliquid  Lorem ipsum dolor sit amet consectetur adipisicing 
+  elit. Nesciunt amet eligendi voluptatum mollitia autem, veritatis dicta dolor
+  emque nulla quibusdam, iusto consequatur eum ad 
+  deleniti tempora et atque nihil voluptate veniam?</p>
+  <div class="modal-btn-container display-flex"> <button class="see-project" type="button">See Live</button>
+    <button class="see-project" type="button">See Source</button></div> 
+       
+    `;  
+    document.getElementById("modal-main-container-id").appendChild(modalContainer);    
+  });
+});
