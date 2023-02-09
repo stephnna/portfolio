e1a4912a866bcdf62ed4584c867e063f0ce95367
Stephen <stephennnabuike@gmail.com>
2023-02-09 11:53:09 +0100

    Add more projects

--- script.js
+++ script.js
@@ -8,10 +8,74 @@ hamBurger.addEventListener('click', () => {
 
 // creating first card item i.e grid project
 
-const projects = [{
+const projects = [
+  {
+    closeButton: 'images/modal-close-button.svg',
+    name: 'Online Hotel Booking',
+    description: 'Online Hotel Booking',
+    imageMobile: 'images/hotel_modal.jpg',
+    imageDesktop: 'images/hotel-modal-desktop.png',
+    rubySkill: 'Ruby on rails',
+    cssSkill: 'Ruby',
+    jsSkill: 'PosgreSQL',
+    htmlSkill: 'React',
+    CodeKit: 'Ruby',
+    gitHub: 'Ruby on rails',
+    boostrap: 'React',
+    terminal: 'Javascript',
+    codePen: 'Redux',
+    seeProject: 'See Project',
+    live: 'https://hotel-frontend-rosy.vercel.app/',
+    source: 'https://github.com/t-bello7/hotel-backend',
+    modalDescription: `Tha goal of this project are as follows:
+    Apply technical knowledge and skills gained in previous modules in a complex project.
+    Understand pros and cons of different approaches of connecting Ruby on Rails back-end with React front-end.
+    Understand principles of Ruby on Rails and React frameworks.
+    Apply Ruby best practices and language style guides in code.
+    Apply RoR best practices and language style guides in code (e.g. thin controllers).
+    Apply JavaScript best practices and language style guides in code.
+    Apply React best practices and language style guides in code.
+    Learn about and practice giving constructive feedback to teammates.
+    Perform a code review for a team member.
+    Use the "Review change"" feature from GitHub.
+    Write clear, professional, and respectful review comments for other team members.
+    Explain "why" a change is requested when giving a code review.
+    Plan a 2+ week project with no interim Microverse-set milestones and submit it on time.
+    Apply knowledge of setting working agreements to set group project teams up for success.
+    Independently implement best practices for group agreements to improve teamwork in larger group project.
+    Demonstrate ability to apply best practices of communication for resolving teamwork challenges.
+    Understand that respect is the foundation of strong relationship-building with teammates.
+    Show up throughout group projects as a reliable and committed team member who communicates and manages your time effectively.
+    Recognize the importance of investing in good working relationships with teammates.
+    Understand principles of strong teamwork (being reliable, committed, and consistent) and how to apply them in group projects.
+    Recognize the value of making equal contributions to group projects to produce the best outcome.
+    Use empty Kanban board to manage tasks with team and own time on the project.`,
+  },
+  {
+    closeButton: 'images/modal-close-button.svg',
+    name: 'Coincript Market',
+    description: 'Crypto Price Tracking App',
+    imageMobile: 'images/coincript_modal.jpg',
+    imageDesktop: 'images/coincript_desktop.png',
+    rubySkill: 'JavaScript',
+    cssSkill: 'React',
+    jsSkill: 'Redux',
+    htmlSkill: 'Jest',
+    CodeKit: 'Codekit',
+    gitHub: 'React',
+    boostrap: 'JavaScript',
+    terminal: 'Jest',
+    codePen: 'React testing Library',
+    seeProject: 'See Project',
+    live: 'https://coincript.netlify.app/',
+    source: 'https://github.com/stephnna/coincrypt-app',
+    modalDescription: 'Coincript is a price-tracking website for digital assets in a fast paced cryptocurrency world. It is useful by making crypto changes discoverable globally thereby empowering retail users with market state for effective trading decision.',
+  
+  },
+  {
   closeButton: 'images/modal-close-button.svg',
   name: 'SkillHub Academy',
-  description: 'SkillHub Academy',
+  description: 'Online Skillhub Academy',
   imageMobile: 'images/skillhub-mobile.png',
   imageDesktop: 'images/skillhub-desktop.png',
   rubySkill: 'JavaScript',
@@ -31,7 +95,7 @@ const projects = [{
 {
   closeButton: 'images/modal-close-button.svg',
   name: 'E Meal',
-  description: 'E Meal',
+  description: 'E Meal booking',
   imageMobile: 'images/meal_mobile.png',
   imageDesktop: 'images/meal-screnshot.png',
   rubySkill: 'JavaScript',
@@ -72,10 +136,10 @@ const projects = [{
 },
 {
   closeButton: 'images/modal-close-button.svg',
-  name: 'React Bookstore App',
-  description: 'React Bookstore App',
-  imageMobile: '',
-  imageDesktop: 'images/desktop-bookstore.png',
+  name: 'Space X App',
+  description: 'Scientific Space App',
+  imageMobile: 'images/spacex_modal.jpg',
+  imageDesktop: 'images/spacex_desktop.png',
   rubySkill: 'React',
   cssSkill: 'Redux',
   jsSkill: 'JavaScript',
@@ -86,74 +150,11 @@ const projects = [{
   terminal: 'Jest',
   codePen: 'GitFlow',
   seeProject: 'See Project',
-  live: 'https://stephen-bookstore.netlify.app/',
-  source: 'https://github.com/stephnna/bookstore',
-  modalDescription: 'The Bookstore is a website where a user can add books, display or remove a selected book. It was built with React and Redux.',
+  live: 'https://space-amejid-stephen.netlify.app/',
+  source: 'https://github.com/amejid/space-travelers-hub',
+  modalDescription: 'This web application provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions.',
 
-},
-{
-  closeButton: 'images/modal-close-button.svg',
-  name: 'Online Hotel Booking',
-  description: 'Online Hotel Booking',
-  imageMobile: 'images/modal-mobile-background.png',
-  imageDesktop: 'images/hotel-modal-desktop.png',
-  rubySkill: 'Ruby on rails',
-  cssSkill: 'Ruby',
-  jsSkill: 'PosgreSQL',
-  htmlSkill: 'React',
-  CodeKit: 'Ruby',
-  gitHub: 'Ruby on rails',
-  boostrap: 'React',
-  terminal: 'Javascript',
-  codePen: 'Redux',
-  seeProject: 'See Project',
-  live: 'https://hotel-frontend-rosy.vercel.app/',
-  source: 'https://github.com/t-bello7/hotel-backend',
-  modalDescription: `Tha goal of this project are as follows:
-  Apply technical knowledge and skills gained in previous modules in a complex project.
-  Understand pros and cons of different approaches of connecting Ruby on Rails back-end with React front-end.
-  Understand principles of Ruby on Rails and React frameworks.
-  Apply Ruby best practices and language style guides in code.
-  Apply RoR best practices and language style guides in code (e.g. thin controllers).
-  Apply JavaScript best practices and language style guides in code.
-  Apply React best practices and language style guides in code.
-  Learn about and practice giving constructive feedback to teammates.
-  Perform a code review for a team member.
-  Use the "Review change"" feature from GitHub.
-  Write clear, professional, and respectful review comments for other team members.
-  Explain "why" a change is requested when giving a code review.
-  Plan a 2+ week project with no interim Microverse-set milestones and submit it on time.
-  Apply knowledge of setting working agreements to set group project teams up for success.
-  Independently implement best practices for group agreements to improve teamwork in larger group project.
-  Demonstrate ability to apply best practices of communication for resolving teamwork challenges.
-  Understand that respect is the foundation of strong relationship-building with teammates.
-  Show up throughout group projects as a reliable and committed team member who communicates and manages your time effectively.
-  Recognize the importance of investing in good working relationships with teammates.
-  Understand principles of strong teamwork (being reliable, committed, and consistent) and how to apply them in group projects.
-  Recognize the value of making equal contributions to group projects to produce the best outcome.
-  Use empty Kanban board to manage tasks with team and own time on the project.`,
-},
-{
-  closeButton: 'images/modal-close-button.svg',
-  name: 'Keeping track of hundreds of components',
-  description: 'Coming soon!',
-  imageMobile: 'images/modal-mobile-background.png',
-  imageDesktop: 'images/modal-desktop-backgroun.png',
-  rubySkill: 'Ruby on rails',
-  cssSkill: 'css',
-  jsSkill: 'JavaScript',
-  htmlSkill: 'html',
-  CodeKit: 'Codekit',
-  gitHub: 'GitHub',
-  boostrap: 'Boostrap',
-  terminal: 'Terminal',
-  codePen: 'Codepen',
-  seeProject: 'See Project',
-  live: 'https://stephnna.github.io/portfolio/',
-  source: 'https://github.com/stephnna/portfolio',
-  modalDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae modi inventore ex aut voluptatum eaque a repellendus animi dolore sed  temporibus autem amet quod, tempore architecto vitae aspernatur minima aliquid  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt amet eligendi voluptatum mollitia autem, veritatis dicta doloremque nulla quibusdam,iusto consequatur eum ad temporibus autem amet quod, tempore architecto vitae aspernatur minima aliquid  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt amet eligendi voluptatum mollitia autem, veritatis dicta doloremque nulla quibusdam, iusto consequatur eum ad deleniti tempora et atque nihil voluptate veniam?',
-
-},
+}
 ];
 
 const sectionElement = document.getElementById('projects-container');
